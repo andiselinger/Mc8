699e8ce089ccf9a1e5ee0cb68a5342d7dac7a19f
Andi <andreasselinger@gmail.com>
2016-05-05 19:46:45 +0200

    spaces instead of tabs!

--- src/main.cpp
+++ src/main.cpp
@@ -10,52 +10,48 @@
 
 using namespace std;
 
+int
+sc_main (int argc, char* argv[])
+{
 
+  //initSesydFramework();
 
-int sc_main(int argc, char* argv[]) {
+  //myWriter = new FileWriter();
 
-	//initSesydFramework();
+  sc_signal<bool> sig_clk;
+  sc_signal<bool> sig_tclk;
+  sc_signal<bool> sig_res;
 
-	//myWriter = new FileWriter();
+  sc_signal<bool> sig_wr;
+  sc_signal<bool> sig_rd;
+  sc_signal<bool> sig_mreq;
+  sc_signal<bool> sig_iorq;
 
+  sc_signal<sc_bv<8> > sig_dataBus;
+  sc_signal<sc_bv<16> > sig_addressBus;
 
-	sc_signal<bool> sig_clk;
-	sc_signal<bool> sig_tclk;
-	sc_signal<bool> sig_res;
+  // Components
+  clkResGen crg ("CLKRES");
+  crg.clk (sig_clk);
+  crg.tclk (sig_tclk);
+  crg.res (sig_res);
 
-	sc_signal<bool> sig_wr;
-	sc_signal<bool> sig_rd;
-	sc_signal<bool> sig_mreq;
-	sc_signal<bool> sig_iorq;
+  mc8 myMC8 ("myMC8");
+  myMC8.clk (sig_clk);
+  myMC8.tclk (sig_tclk);
+  myMC8.res (sig_res);
+  myMC8.wr (sig_wr);
+  myMC8.rd (sig_rd);
+  myMC8.mreq (sig_mreq);
+  myMC8.iorq (sig_iorq);
+  myMC8.dataBus (sig_dataBus);
+  myMC8.addressBus (sig_addressBus);
 
-	sc_signal<sc_bv<8> > sig_dataBus;
-	sc_signal<sc_bv<16> > sig_addressBus;
-
-
-
-	// Components
-	clkResGen crg("CLKRES");
-	crg.clk(sig_clk);
-	crg.tclk(sig_tclk);
-	crg.res(sig_res);
-
-	mc8 myMC8("myMC8");
-	myMC8.clk(sig_clk);
-	myMC8.tclk(sig_tclk);
-	myMC8.res(sig_res);
-	myMC8.wr(sig_wr);
-	myMC8.rd(sig_rd);
-	myMC8.mreq(sig_mreq);
-	myMC8.iorq(sig_iorq);
-	myMC8.dataBus(sig_dataBus);
-	myMC8.addressBus(sig_addressBus);
-
-	asyncRom myRom("myROM");
-	myRom.rd(sig_rd);
-	myRom.addrBus(sig_addressBus);
-	myRom.dataBus(sig_dataBus);
-	myRom.initMemory();
-
+  asyncRom myRom ("myROM");
+  myRom.rd (sig_rd);
+  myRom.addrBus (sig_addressBus);
+  myRom.dataBus (sig_dataBus);
+  myRom.initMemory ();
 
 //	sc_trace_file *fp;
 //	fp=sc_create_vcd_trace_file("wave");
@@ -69,14 +65,14 @@ int sc_main(int argc, char* argv[]) {
 //	sc_trace(fp,sig_dataBus,"dataBus");
 //	sc_trace(fp,sig_addressBus,"addressBus");
 
-	//myWriter->setupVCDFile("mc8");
+//myWriter->setupVCDFile("mc8");
 
-	sc_start(200, SC_MS);	// run the simulation for 100 µ-sec
+  sc_start (200, SC_MS);	// run the simulation for 100 µ-sec
 
-	//myWriter->closeFile();
+  //myWriter->closeFile();
 //	sc_close_vcd_trace_file(fp);
 
-	return 0;
+  return 0;
 
 }
 ;
