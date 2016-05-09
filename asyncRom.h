a351a208d4ea2b6b2cebf8d8659d651ec6ca59f2
wolfg1230 <wolfg1230@users.noreply.github.com>
2016-05-09 15:56:00 +0200

    Add files via upload

---
+++ asyncRom.h
@@ -0,0 +1,49 @@
+#include "systemc.h"
+
+#define ROM_READ_DELAY_NS 500
+
+SC_MODULE(asyncRom) {
+
+public:
+	sc_in<bool> rd;
+	sc_in<sc_bv<16> > addrBus;
+	sc_out<sc_bv<8> > dataBus;
+
+	SC_CTOR(asyncRom) {
+		SC_THREAD(readData);
+
+	}
+
+	void initMemory() {
+		memory[0] = 0x3A;
+		memory[1] = 0x03;
+		memory[2] = 0x00;
+		memory[3] = 0xFF;
+		memory[4] = 0x22;
+		memory[0x2205] = 0x3D;
+		memory[0x2206] = 0xC3;
+		memory[0x2207] = 0x02;
+		memory[0x2208] = 0x00;
+		memory[0x2209] = 0xC3;
+		memory[0x220A] = 0x05;
+		memory[0x220B] = 0x22;
+
+
+	}
+
+private:
+
+	sc_bv<16> memory[65535];
+	unsigned int addressToRead;
+
+	void readData() {
+		while (1) {
+			wait(rd.negedge_event());
+			addressToRead = addrBus.read().to_uint();
+			wait(ROM_READ_DELAY_NS, SC_US);
+			cout << "ROM Data ready." << endl;
+			dataBus.write(memory[addressToRead]);
+		}
+	}
+
+};
